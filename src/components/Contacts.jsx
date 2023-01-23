1ef752621cb90d72627b6d27786d8cfa417c8af0
Brobot64 <50984975+Brobot64@users.noreply.github.com>
2023-01-23 08:53:41 +0100

    contact
    changes to title

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -27,7 +27,7 @@ export default function Contacts({ contacts, currentUser, changeChat }) {
           <Container>
             <div className="brand">
               <img src={Logo} alt="logo" />
-              <h3>Snappy</h3>
+              <h3>B Chat</h3>
             </div>
 
             <div className="contacts">
